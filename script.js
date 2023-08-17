f749254861e779ae15290b274574bb32b4bed0ca
Crihojames <crihojames@gmail.com>
2023-08-17 20:19:09 +0100

    Définir les fonctions auxiliaires

--- script.js
+++ script.js
@@ -27,3 +27,77 @@ const form = document.querySelector('form');
 form.addEventListener('submit',event =>{
     event.preventDefault();
 });
+
+// Définir les fonctions auxiliaires
+
+// Une fonction qui met à jour l'affichage de l'entrée et du calcul
+function updateDisplay() {
+  input.value = inputNumber; // Afficher le nombre saisi dans l'élément input
+  calcul.textContent = calculationString; // Afficher le calcul en cours dans l'élément p
+}
+
+// Une fonction qui réinitialise tout
+function resetAll() {
+  inputNumber = ""; // Réinitialiser la chaîne du nombre saisi
+  inputOperator = ""; // Réinitialiser la chaîne de l'opérateur saisi
+  calculationString = ""; // Réinitialiser la chaîne du calcul en cours
+  result = 0; // Réinitialiser le résultat du calcul
+  updateDisplay(); // Mettre à jour l'affichage
+}
+
+// Une fonction qui efface l'entrée actuelle
+function clearInput() {
+  inputNumber = ""; // Effacer la chaîne du nombre saisi
+  updateDisplay(); // Mettre à jour l'affichage
+}
+
+// Une fonction qui change le signe de l'entrée actuelle
+function changeSign() {
+  if (inputNumber) {
+    // Si la chaîne du nombre saisi n'est pas vide
+    inputNumber = (-inputNumber).toString(); // Convertir le nombre saisi en son opposé et le reconvertir en chaîne
+    updateDisplay(); // Mettre à jour l'affichage
+  }
+}
+
+// Une fonction qui renvoie le résultat du calcul en pourcentage
+function getPercentage() {
+  if (calculationString) {
+    // Si la chaîne du calcul en cours n'est pas vide
+    result /= 100; // Diviser le résultat par 100 pour obtenir le pourcentage
+    inputNumber = result.toString(); // Convertir le résultat en chaîne et l'affecter à la chaîne du nombre saisi
+    calculationString += " %"; // Ajouter le symbole du pourcentage à la fin de la chaîne du calcul en cours
+    updateDisplay(); // Mettre à jour l'affichage
+  }
+}
+
+// Une fonction qui ajoute un opérateur au calcul en cours
+function addOperator(operator) {
+  if (inputNumber) {
+    // Si la chaîne du nombre saisi n'est pas vide
+    if (calculationString) {
+      // Si la chaîne du calcul en cours n'est pas vide
+      calculationString += ` ${inputOperator} ${inputNumber}`; // Ajouter l'opérateur et le nombre saisis à la fin de la chaîne du calcul en cours
+      result = eval(calculationString); // Évaluer la chaîne du calcul en cours et affecter le résultat à la variable result
+    } else {
+      // Si la chaîne du calcul en cours est vide
+      calculationString = inputNumber; // Affecter le nombre saisi à la chaîne du calcul en cours
+      result = Number(inputNumber); // Convertir le nombre saisi en nombre et l'affecter à la variable result
+    }
+    inputNumber = ""; // Réinitialiser la chaîne du nombre saisi
+    inputOperator = operator; // Affecter l'opérateur passé en paramètre à la chaîne de l'opérateur saisi
+    updateDisplay(); // Mettre à jour l'affichage
+  }
+}
+
+// Une fonction qui effectue le calcul et affiche le résultat
+function calculate() {
+  if (inputNumber && inputOperator) {
+    // Si la chaîne du nombre saisi et la chaîne de l'opérateur saisi ne sont pas vides
+    calculationString += ` ${inputOperator} ${inputNumber}`; // Ajouter l'opérateur et le nombre saisis à la fin de la chaîne du calcul en cours
+    result = eval(calculationString); // Évaluer la chaîne du calcul en cours et affecter le résultat à la variable result
+    inputNumber = result.toString(); // Convertir le résultat en chaîne et l'affecter à la chaîne du nombre saisi
+    calculationString += " ="; // Ajouter le symbole d'égalité à la fin de la chaîne du calcul en cours
+    updateDisplay(); // Mettre à jour l'affichage
+  }
+}
