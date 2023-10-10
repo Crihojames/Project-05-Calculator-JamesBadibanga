96ae539cdc0a6db0281ff9373c4b18c399fbc7b2
Crihojames <crihojames@gmail.com>
2023-10-10 20:18:06 +0100

    mis a jour 1

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
-//  import { calculate } from './calculator.js';
-
-// TODO: Faire la manipulation du DOM dans ce fichier
 
 // Sélectionner les éléments du DOM
-const input = document.getElementById("input"); // L'élément input qui affiche le nombre saisi
+const input = document.getElementById("input");
 const calcul = document.getElementById("calcul"); // L'élément p qui affiche le calcul en cours
-const reset = document.getElementById("reset"); // Le bouton AC qui réinitialise tout
-const clear = document.getElementById("clear"); // Le bouton C qui efface l'entrée actuelle
-const plusoumoins = document.getElementById("plusoumoins"); // Le bouton +/- qui change le signe de l'entrée actuelle
-const percentage = document.getElementById("percentage"); // Le bouton % qui renvoie le résultat du calcul en pourcentage
-const divideby = document.getElementById("divideby"); // Le bouton ÷ qui ajoute l'opérateur de division au calcul
-const times = document.getElementById("times"); // Le bouton × qui ajoute l'opérateur de multiplication au calcul
-const minus = document.getElementById("minus"); // Le bouton - qui ajoute l'opérateur de soustraction au calcul
-const plus = document.getElementById("plus"); // Le bouton + qui ajoute l'opérateur d'addition au calcul
-const equals = document.getElementById("equals"); // Le bouton = qui effectue le calcul et affiche le résultat
+const reset = document.getElementById("reset");
+const clear = document.getElementById("clear");
+const plusoumoins = document.getElementById("plusoumoins");
+const percentage = document.getElementById("percentage");
+const divideby = document.getElementById("divideby");
+const times = document.getElementById("times");
+const minus = document.getElementById("minus");
+const plus = document.getElementById("plus");
+const equals = document.getElementById("equals");
 const digits = document.querySelectorAll(".digit"); // Les boutons des chiffres de 0 à 9
-const dot = document.querySelector(".dot"); // Le bouton du point décimal
+const dot = document.querySelector(".dot");
 let inputcurseur = document.querySelector('input');
 const digitZeroTwo = document.getElementById("twoZero");
 const pi = document.getElementById("pi");
@@ -23,16 +20,19 @@ const pi = document.getElementById("pi");
 // Masque le curseur clignotant By yvonne
 inputcurseur.style.caretColor = 'transparent';
 
-// Déclarer les variables globales
-let inputNumber = ""; // La chaîne qui stocke le nombre saisi par l'utilisateur
-let inputOperator = ""; // La chaîne qui stocke l'opérateur saisi par l'utilisateur
-let calculationString = ""; // La chaîne qui stocke le calcul en cours
-let result = 0; // Le nombre qui stocke le résultat du calcul
-
+//
 const form = document.querySelector('form');
 form.addEventListener('submit',event =>{
     event.preventDefault();
 });
+
+
+// Déclarer les variables globales
+let inputNumber = "";
+let inputOperator = "";
+let calculationString = ""; // La chaîne qui stocke le calcul en cours
+let result = 0; // Le nombre qui stocke le résultat du calcul
+
 
 // Définir les fonctions auxiliaires
 
